b6c5f143ea064d2e979ecfb195045598cef4b11d
Efe <sidalefe2005@gmail.com>
2026-01-22 13:22:15 +0300

    Update capture.py

--- src/capture.py
+++ src/capture.py
@@ -6,26 +6,25 @@ def start_sniffer(interface, count=100):
     """
     Belirtilen arayüzden paket yakalar ve 'data/captures' klasörüne kaydeder.
     """
-    # 1. Klasör Yolunu Belirle (data/captures)
     base_dir = "data"
     capture_dir = os.path.join(base_dir, "captures")
     
-    # 2. Klasör Yoksa Oluştur (Hata almamak için)
+    
     if not os.path.exists(capture_dir):
         os.makedirs(capture_dir)
     
-    # 3. Dosya İsmi Oluştur (Zaman damgalı: capture_20250121_1530.pcap)
+    
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     filename = f"capture_{timestamp}.pcap"
-    filepath = os.path.join(capture_dir, filename) # Tam yol: data/captures/capture_...
+    filepath = os.path.join(capture_dir, filename) 
     
     print(f"[*] {interface} uzerinden dinleme baslatildi... ({count} paket)")
     
     try:
-        # Paketleri Yakala
+       
         packets = sniff(iface=interface, count=count)
         
-        # Dosyayı Kaydet
+        
         wrpcap(filepath, packets)
         print(f"[+] Paketler kaydedildi: {filepath}")
         return filepath
